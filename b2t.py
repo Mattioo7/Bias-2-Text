e8272b27972232a66bbb430e4229bd7e2e1b9a9b
Mateusz Chudek <mateusz.chudek.01@gmail.com>
2026-08-30 12:36:05 +0100

    Added MultiCap

--- b2t.py
+++ b2t.py
@@ -32,7 +32,7 @@ import warnings
 from torch.serialization import SourceChangeWarning
 warnings.filterwarnings("ignore", category=SourceChangeWarning)
 
-all_captioning_models = ["clipcap", "gpt-4o", "gpt-4o-mini"]
+all_captioning_models = ["clipcap", "multicap", "gpt-4o", "gpt-4o-mini"]
 all_keyword_extraction_models = ["yake", "gpt-4o", "gpt-4o-mini"]
 all_datasets = ['waterbird', 'celeba']
 
